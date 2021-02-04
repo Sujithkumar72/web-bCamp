3ea0afe66325975bba8a2155e8e72ea732a62edf
Sujith kumar <k.sujithkumar@outlook.com>
2021-02-04 14:32:30 +0530

    multipage Todo list completed

--- todo-v1/app.js
+++ todo-v1/app.js
@@ -3,30 +3,53 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("server started at 3000");
 });
 
-var newtasks =["coffee","cake","muffins"];
-app.get("/", function(req, res){
-  var today= new Date();
-  var dateSettings = {
-    weekday:"long",
-    day:"numeric",
-    month:"long",
-    year:"numeric",
-  }
-  var dayFull = today.toLocaleDateString("en-IN", dateSettings);
+var newtasks = ["coffee", "cake", "muffins"];
+var workitems = [];
+var dayFull = "";
 
-      res.render("list", {kindOfDay:dayFull, tasknew:newtasks});
+app.get("/", function(req, res) {
+  var today = new Date();
+  var dateSettings = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }
+  dayFull = today.toLocaleDateString("en-IN", dateSettings);
+
+  res.render("list", {
+    listTitle: dayFull,
+    tasknew: newtasks
+  });
 });
 
-app.post("/", function(req, res){
-    var newtask = req.body.task;
-    newtasks.push(newtask);
+app.get("/work", function(req, res) {
+
+  res.render("list", {
+    listTitle: "Work List",
+    tasknew: workitems
+  });
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.task;
+  console.log(req.body);
+  if (req.body.submitName === dayFull) {
+    newtasks.push(item);
     res.redirect("/");
+  } else {
+    workitems.push(item);
+    res.redirect("/work");
+  }
+
 });
