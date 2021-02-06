e16ad38ada745e90477e19d25c1199fe1e425cf8
Sujith kumar <k.sujithkumar@outlook.com>
2021-02-06 14:11:03 +0530

    challenge 11 completed

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -9,36 +9,33 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
-app.get("/", function(req,res){
-  res.render("home",{homeStartingContentejs: homeStartingContent});
+app.get("/", function(req, res) {
+  res.render("home", {homeStartingContentejs: homeStartingContent});
 })
-
-app.get("/about", function(req, res){
-  res.render("about",{aboutContentejs:aboutContent});
+app.get("/about", function(req, res) {
+  res.render("about", {aboutContentejs: aboutContent});
 });
-
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContentejs: contactContent});
+app.get("/contact", function(req, res) {res.render("contact", {contactContentejs: contactContent});
 });
-
-app.get("/compose", function(req,res){
+app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.post("/",function(req, res){
-  let newcomposed = req.body.composeejs;
-  console.log(newcomposed);
-})
+var posts =[];
 
+app.post("/compose", function(req, res) {
 
+  let post = {
+    postTitle: req.body.newtitle,
+    postBody: req.body.newPost
+  };
+  console.log(post);
+  res.redirect("/");
+});
 
 
 
