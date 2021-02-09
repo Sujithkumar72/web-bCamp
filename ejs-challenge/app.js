7f18269f55acf1116e00235a37e6ca3ce8c99515
Sujith kumar <k.sujithkumar@outlook.com>
2021-02-09 14:28:54 +0530

    challenge 18 completed

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ =require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -19,7 +20,6 @@ let posts =[];
 
 app.get("/", function(req, res) {
     res.render("home", {homeStartingContentejs: homeStartingContent, blogPosts: posts});
-    console.log(posts);
 })
 app.get("/about", function(req, res) { res.render("about", {aboutContentejs: aboutContent});});
 app.get("/contact", function(req, res) {res.render("contact", {contactContentejs: contactContent});});
@@ -37,16 +37,20 @@ app.post("/compose", function(req, res) {
 });
 
 app.get("/posts/:dayPost",function(req,res){
-let pageRequested = req.params.dayPost;
-
+let pageRequested = _.lowerCase(req.params.dayPost);
 posts.forEach(function(postBlog){
-  let checkPost = postBlog.postTitle;
-  if (checkPost === pageRequested){
+  let checkPost = _.lowerCase(postBlog.postTitle);
+  console.log(checkPost + "the title of the blog post");
+  console.log("the page you requested is " + pageRequested);
 
-     console.log("Match found");
+  if (checkPost === pageRequested){
+console.log("Match found");
+
+   } else {
+    console.log("No Match found");
    }
  });
-
+ 
 });
 
 
