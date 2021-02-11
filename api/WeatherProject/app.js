81a22148d9ea991b3879d7d155508a3631aa2e50
Sujith kumar <k.sujithkumar@outlook.com>
2021-02-11 18:34:12 +0530

    JSON data parsing

--- api/WeatherProject/app.js
+++ api/WeatherProject/app.js
@@ -4,11 +4,25 @@ const https = require("https");
 const app =express();
 
 app.get("/",function(req,res){
-const  url = "https://api.openweathermap.org/data/2.5/weather?q=Chennai&appid=67308674280fbc3b9f2aa299fc0bc8b0";
-  https.get(url, function(res){
+const  url = "https://api.openweathermap.org/data/2.5/weather?q=Chennai&units=metric&appid=67308674280fbc3b9f2aa299fc0bc8b0";
+  https.get(url, function(response){
     console.log(res.statusCode);
+    response.on("data", function(data){
+
+      const weatherdata=JSON.parse(data);
+      console.log(weatherdata);
+      const temp = weatherdata.main.temp;
+      const weathDescription = weatherdata.weather[0].description;
+        const iconImg = weatherdata.weather[0].icon;
+        const imageURL = "http://openweathermap.org/img/wn/"+iconImg+"@2x.png";
+      console.log(imageURL);
+      res.write("<p>The weather is currently is " + weathDescription+"</p>");
+      res.write("<h1>The temperature of Chennai now is "+ temp + ".</h1>");
+      res.write("<a href="+imageURL+"><img src="+imageURL+"></a>");
+      res.send();
+    })
   });
-  res.send("Server is Up and running");
+
 });
 
 app.listen(3000, function(){
