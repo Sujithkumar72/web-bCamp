f7136eb0a6490603a51797f2d821273beb96f714
Sujith kumar <k.sujithkumar@outlook.com>
2021-02-16 14:17:57 +0530

    Mongoose insert completed

--- fruits/app.js
+++ fruits/app.js
@@ -1,51 +1,62 @@
-const MongoClient = require("mongodb").MongoClient
-const assert = require("assert");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/fruitsDB",{useNewUrlParser: true, useUnifiedTopology: true});
 
-
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri =
-  "mongodb://localhost:27017";
-
-const dbName = "fruitsDB";
-
-const client = new MongoClient(uri, {
-  useUnifiedTopology: true
+const fruitSchema = new mongoose.Schema({
+  name: String,
+  rating: Number,
+  review:String
 });
 
-client.connect(function(err) {
-  assert.equal(null, err);
-  console.log("connected successfully to server");
+const Fruite = mongoose.model("fruit", fruitSchema);
+const fruit = new Fruite({
+  name:"Apple",
+  rating:7,
+  review: "expensive"
+})
+// fruit.saves //saves single entry to the database.collection
 
-  const db = client.db(dbName);
-  
-  findDocuments(db,function(){
-      client.close();
-  })
-
+const kiwi = new Fruite({
+  name:"kiwi",
+  rating: 8,
+  review:"Awesome"
+})
+const orange = new Fruite({
+  name: "Orange",
+  rating:9,
+  review:"citric"
+})
+/* insert multiple fruits to the database
+Fruite.insertMany([kiwi, orange], function(err){
+  if (err){
+    console.log(err);
+  }
+  else{
+    console.log("Successfully saved all the fruits in fruitsDB.");
+  }
+})
+*/
+const personSchema = new mongoose.Schema({
+  name:String,
+  age:Number
 })
 
+const Person = mongoose.model("person", personSchema);
 
-const insertDocuments = function(db, callback) {
-  const collection = db.collection("fruits");
-  collection.insertMany([{
-      name: "Apple",
-      score: 8,
-      review: "Great Fruit"
-    }, {
-      name: "Orange",
-      score: 7.5,
-      review: "kinda sour"
-    }, {
-      name: "Banana",
-      score: 9,
-      review: "bananaaaa"
-    }],
-    function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-    })
-}
+const john = new Person({
+  name:"John",
+  age: 37
+})
+const sujith= new Person({
+  name:"sujith",
+  age:30
+})
+// sujith.save(); // save single entry to the database.collection
+
+
+
+
+
+
 
 const findDocuments = function(db, callback){
   const collection = db.collection("fruits");
