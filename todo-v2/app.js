eb2a796222169376e1fd69a41b982939a4bcfed0
Sujith kumar <k.sujithkumar@outlook.com>
2021-03-24 19:29:12 +0530

    atlAS CONNECTED AND ADD

--- todo-v2/app.js
+++ todo-v2/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 const mongoose= require("mongoose");
+const url =require("./key");
 
 const app = express();
 
@@ -12,23 +13,45 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// const items = ["Buy Food", "Cook Food", "Eat Food"];
-// const workItems = [];
+//mongodb connection using mongoose
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true });
 
+//mongodb Schema
+const itemsSchema = { 
+  name : String
+};
+
+//mongoose model to create collection
+const Item = mongoose.model('Item',itemsSchema);
+
+//mongodb document using Model in the Collection
+const item1 = new Item({
+  name: "Welcome to your Todo"
+});
+const item2 = new Item({
+  name:"Hit the + to add new item"
+});
+const item3 = new Item({
+  name:"click <-- to delete the item"
+});
+const defaultItems =[item1, item2, item3]; //array of document objects
+
+//Inserting array into collection using Model as Documents
+Item.insertMany(defaultItems, function(err){
+  if(err){
+    console.log(err);
+  } else{
+    console.log("Default items added to the DB");
+  }
+});
 
 
 app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+  res.render("list", {listTitle: "Today", newListItems: items});
 });
 
 app.post("/", function(req, res){
-
   const item = req.body.newItem;
-
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
