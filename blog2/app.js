0b9e3ad3e9f57cefc4bb8be597f5ad73e98bb9e8
Sujith kumar <k.sujithkumar@outlook.com>
2021-03-24 16:25:50 +0530

    revokd

--- blog2/app.js
+++ blog2/app.js
@@ -2,21 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const _ = require("lodash");
 const ejs = require("ejs");
-const MongoClient = require('mongodb').MongoClient;
+
 
 const app=express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-const uri = "mongodb+srv://admin-sujith:Test_123@cluster0.8ffvv.mongodb.net/blog?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("blog").collection("devices");
-  // perform actions on the collection object
-  console.log("Connection successful");
-  client.close();
-});
+
 
 
 const homeStartingContent ="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum";
